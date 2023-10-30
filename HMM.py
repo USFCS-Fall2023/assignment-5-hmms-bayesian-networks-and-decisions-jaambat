53ab4e4f489a09ff29036cd7a59d2e74be1491e6
jaambat <jaambat@dons.usfca.edu>
2023-10-29 19:06:17 -0700

    set up the submission.py, and requirements.txt.

--- HMM.py
+++ HMM.py
@@ -1,4 +1,8 @@
-
+"""
+James Ambat
+CS-462
+Assignment 5
+"""
 
 import random
 import argparse
@@ -6,23 +10,28 @@ import codecs
 import os
 import numpy
 
+
 # observations
 class Observation:
     def __init__(self, stateseq, outputseq):
-        self.stateseq  = stateseq   # sequence of states
+        self.stateseq = stateseq  # sequence of states
         self.outputseq = outputseq  # sequence of outputs
+
     def __str__(self):
-        return ' '.join(self.stateseq)+'\n'+' '.join(self.outputseq)+'\n'
+        return ' '.join(self.stateseq) + '\n' + ' '.join(self.outputseq) + '\n'
+
     def __repr__(self):
         return self.__str__()
+
     def __len__(self):
         return len(self.outputseq)
+
 
 # hmm model
 class HMM:
     def __init__(self, transitions={}, emissions={}):
         """creates a model from transition and emission probabilities"""
-        ## Both of these are dictionaries of dictionaries. e.g. :
+        # Both of these are dictionaries of dictionaries. e.g. :
         # {'#': {'C': 0.814506898514, 'V': 0.185493101486},
         #  'C': {'C': 0.625840873591, 'V': 0.374159126409},
         #  'V': {'C': 0.603126993184, 'V': 0.396873006816}}
@@ -30,30 +39,21 @@ class HMM:
         self.transitions = transitions
         self.emissions = emissions
 
-    ## part 1 - you do this.
+    # part 1 - you do this.
     def load(self, basename):
         """reads HMM structure from transition (basename.trans),
         and emission (basename.emit) files,
         as well as the probabilities."""
 
-
-
-   ## you do this.
+    # you do this.
     def generate(self, n):
         """return an n-length observation by randomly sampling from this HMM."""
 
-
-
-    ## you do this: Implement the Viterbi alborithm. Given an Observation (a list of outputs or emissions)
-    ## determine the most likely sequence of states.
+    # you do this: Implement the Viterbi alborithm. Given an Observation (a list of outputs or emissions)
+    # determine the most likely sequence of states.
 
     def viterbi(self, observation):
         """given an observation,
         find and return the state sequence that generated
         the output sequence, using the Viterbi algorithm.
         """
-
-
-
-
-
---
+++ submission.py
@@ -0,0 +1,23 @@
+"""
+James Ambat
+CS-462
+Assignment 5
+"""
+
+"""
+cd assignment-5-hmms-bayesian-networks-and-decisions-jaambat
+mkdir venv
+python3 -m venv ./venv
+source venv/bin/activate
+
+PyCharm -> Preferences -> Project -> Python Interpreter
+Add interpreter -> Add local interpreter
+"""
+
+
+# To run my submission file:
+"""
+source venv/bin/activate
+pip install -r requirements.txt
+python3 submission.py
+"""
