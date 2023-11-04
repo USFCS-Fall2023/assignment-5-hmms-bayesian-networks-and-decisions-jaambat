ecca57742862ee6916058645898f58a2f90ce8ba
jaambat <jaambat@dons.usfca.edu>
2023-11-04 13:11:41 -0700

    drafted viterbi and backpointer matrix.

--- HMM.py
+++ HMM.py
@@ -234,11 +234,114 @@ class HMM:
     # you do this: Implement the Viterbi algorithm. Given an Observation (a list of outputs or emissions)
     # determine the most likely sequence of states.
 
-    def viterbi(self, observation):
-        """given an observation,
-        find and return the state sequence that generated
-        the output sequence, using the Viterbi algorithm.
+    def viterbi(self, observations: list[str]) -> list[str]:
         """
+        Determines the most likely sequence of states given a list of observations.
+        :param observations: list[str] of a sequence of observations
+        :return: list[str]
+        """
+
+        # Acquire starting probabilities, and prepared indexes for the starting state back pointers
+        starting_probabilities_map = None
+        for item in self.transitions.items():
+            if item[0] == "#":
+                starting_probabilities_map = item[1]
+                break
+
+        state_indexes = {}
+        i = 1
+        for item in list(starting_probabilities_map):
+            state_indexes[item] = i
+            i += 1
+
+        # Initialize viterbi matrix and back pointer matrix, each row will be labeled by the state name as a key
+        column_names = ["-"]
+        column_names.extend(observations)  # column names: ["-", "observation_1", "observation_2", ... ]
+        viterbi_matrix = {}
+        back_pointer_matrix = {}
+        for item in starting_probabilities_map.items():
+            if viterbi_matrix.get(item[0]) is None:
+                # Initialize each row in the viterbi matrix and add column for starting probabilities.
+                viterbi_matrix[item[0]] = [None for _ in range(len(observations) + 1)]
+                viterbi_matrix[item[0]][0] = item[1]
+
+                # Initialize each row in back pointer matrix and leave the first column empty.
+                back_pointer_matrix[item[0]] = [None for _ in range(len(observations) + 1)]
+                back_pointer_matrix[item[0]][0] = None
+
+        # ACQUIRE MAX OF BAYES RULE, loop through each column and fill in viterbi_matrix[i, j] and back_pointer_matrix
+        for j in range(1, len(column_names)):
+            curr_observation = column_names[j]
+
+            # Calculate P(curr_state | e_n, .... e1) for each of the states
+            for item in viterbi_matrix.items():
+                curr_state = item[0]
+                p_curr_state_given_curr_observation = 0
+
+                if j == 1:
+                    p_curr_observation_given_curr_state = None
+                    try:
+                        p_curr_observation_given_curr_state = self.emissions[curr_state][curr_observation]
+                    except KeyError:
+                        # Case where there is no such state for the current observation.
+                        # "i" can never be ADV based on the supplied emissions
+                        p_curr_observation_given_curr_state = 0
+
+                    matrix_row = item[1]
+                    p_curr_state = matrix_row[0]
+
+                    # Apply Bayes rule and record the entry into the viterbi_matrix
+                    p_curr_state_given_curr_observation = p_curr_observation_given_curr_state * p_curr_state
+                    viterbi_matrix[curr_state][j] = p_curr_state_given_curr_observation
+
+                    # Initialize entry in the first column of back pointer matrix with 0
+                    back_pointer_matrix[curr_state][j] = 0
+                    continue
+
+                # For all possible states, acquire P(curr_state | e_n, .... e1) from the emissions and transitions
+                bayes_rule_per_state = []
+                for state in viterbi_matrix.items():
+                    state_i = state[0]
+
+                    p_curr_observation_given_curr_state = None
+                    try:
+                        p_curr_observation_given_curr_state = self.emissions[curr_state][curr_observation]
+                    except KeyError:
+                        # Case where there is no such observation for the given state
+                        p_curr_observation_given_curr_state = 0
+
+                    p_curr_state_given_state_i = self.transitions[state_i][curr_state]
+                    p_state_i_prev_column = viterbi_matrix[state_i][j - 1]
+
+                    # Apply Bayes rule for each state we loop through.
+                    curr_bayes_rule = (p_curr_observation_given_curr_state
+                                       * p_curr_state_given_state_i
+                                       * p_state_i_prev_column)
+
+                    # Save the Bayes Rule output for getting the max AFTER all states have been processed.
+                    bayes_rule_per_state.append({state_i: curr_bayes_rule})
+
+                # Acquire the maximum Bayes Rule output
+                max_state_name = "None"
+                max_state_bayes_rule_value = 0
+                for entry in bayes_rule_per_state:
+                    state_name = list(entry.keys())[0]
+                    curr_bayes_rule_value = entry[state_name]
+
+                    if curr_bayes_rule_value > max_state_bayes_rule_value:
+                        max_state_name = state_name
+                        max_state_bayes_rule_value = curr_bayes_rule_value
+
+                # Update viterbi matrix
+                p_curr_state_given_curr_observation = max_state_bayes_rule_value
+                viterbi_matrix[curr_state][j] = p_curr_state_given_curr_observation
+
+                # Update back_pointer matrix
+                back_pointer_matrix[curr_state][j] = state_indexes[max_state_name]
+
+        # viterbi and back_pointer matrix filled, walk back from back to front of back_pointer
+
+
 
     def read_in_observations(self, observation_file_name: str):
         """
@@ -319,6 +422,13 @@ generate_flag.add_argument("--forward",
                            type=str,
                            help="Runs the forward algorithm on a file of observations and for each observation, "
                                 "the algorithm will provide the most likely final state.")
+
+# Add argument flag for --viterbi and only allow this flag with mutually exclusive
+viterbi_flag = parser.add_mutually_exclusive_group()
+generate_flag.add_argument("--viterbi",
+                           type=str,
+                           help="Runs the viterbi algorithm on a file of observations and for each observation, "
+                                "the algorithm will determine the most likely sequence of states.")
 cli_args = None
 
 try:
@@ -354,11 +464,20 @@ if __name__ == "__main__":
     if cli_args.forward is not None and cli_args.file is not None:
         # Supports forward() in the command line
         observation_file_name = cli_args.forward
-        observations_list = hidden_markov.read_in_observations(observation_file_name)
+        observations_list = hidden_markov.read_in_observations(observation_file_name=observation_file_name)
         print("** Observations from file [  %s  ] **\n%s\n" % (observation_file_name, observations_list))
 
         for observations in observations_list:
             # Take the observation sequences from each row and run forward on them
             most_likely_final_state = hidden_markov.forward(observation_sequence=observations)
             print(most_likely_final_state)
+
+    if cli_args.viterbi is not None and cli_args.file is not None:
+        # Supports viterbi() in the command line
+        observation_file_name = cli_args.viterbi
+        observations_list = hidden_markov.read_in_observations(observation_file_name=observation_file_name)
+
+        for observations in observations_list:
+            # Take the observation sequences from each row and run viterbi on them
+            hidden_markov.viterbi(observations=observations)
 
