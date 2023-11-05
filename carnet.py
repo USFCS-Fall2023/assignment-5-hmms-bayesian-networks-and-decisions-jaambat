a3ea230c444a8fafb32d01d151bf2704a2ef6071
jaambat <jaambat@dons.usfca.edu>
2023-11-05 00:59:33 -0700

    Implemented Part 3.3, for carnet and verified CPD probability outputs.

--- carnet.py
+++ carnet.py
@@ -127,5 +127,121 @@ print("\n"
 print(car_infer.query(variables=["Starts"], evidence={"Radio": "turns on", "Gas": "Full"}))
 print("** The probability that the car starts: 0.7212 **")
 
+# Part 3.3
+# Remaking the Bayesian Network so that previous probabilities are not affected.
+car_model = BayesianNetwork(
+    [
+        ("Battery", "Radio"),
+        ("Battery", "Ignition"),
+        ("Ignition", "Starts"),
+        ("Gas", "Starts"),
+        ("Starts", "Moves"),
+        ("KeyPresent", "Starts")  # Added the KeyPresent to Starts Edge
+    ]
+)
 
+# Added the KeyPresent Node
+cpd_key_present = TabularCPD(
+    variable="KeyPresent", variable_card=2, values=[[0.70], [0.30]],
+    state_names={"KeyPresent": ["yes", "no"]},
+)
+
+cpd_battery = TabularCPD(
+    variable="Battery", variable_card=2, values=[[0.70], [0.30]],
+    state_names={"Battery": ["Works", "Doesn't work"]},
+)
+
+cpd_gas = TabularCPD(
+    variable="Gas", variable_card=2, values=[[0.40], [0.60]],
+    state_names={"Gas": ["Full", "Empty"]},
+)
+
+cpd_radio = TabularCPD(
+    variable="Radio", variable_card=2,
+    values=[[0.75, 0.01], [0.25, 0.99]],
+    evidence=["Battery"],
+    evidence_card=[2],
+    state_names={"Radio": ["turns on", "Doesn't turn on"],
+                 "Battery": ["Works", "Doesn't work"]}
+)
+
+cpd_ignition = TabularCPD(
+    variable="Ignition", variable_card=2,
+    values=[[0.75, 0.01], [0.25, 0.99]],
+    evidence=["Battery"],
+    evidence_card=[2],
+    state_names={"Ignition": ["Works", "Doesn't work"],
+                 "Battery": ["Works", "Doesn't work"]}
+)
+
+# Updated Starts Node
+cpd_starts = TabularCPD(
+    variable="Starts",
+    variable_card=2,
+    values=[[0.99, 0.01, 0.01, 0.01, 0.01, 0.01, 0.01, 0.01], [0.01, 0.99, 0.99, 0.99, 0.99, 0.99, 0.99, 0.99]],
+    evidence=["Ignition", "Gas", "KeyPresent"],
+    evidence_card=[2, 2, 2],
+    state_names={"Starts": ["yes", "no"], "Ignition": ["Works", "Doesn't work"], "Gas": ["Full", "Empty"],
+                 "KeyPresent": ["yes", "no"]},
+)
+
+cpd_moves = TabularCPD(
+    variable="Moves", variable_card=2,
+    values=[[0.8, 0.01], [0.2, 0.99]],
+    evidence=["Starts"],
+    evidence_card=[2],
+    state_names={"Moves": ["yes", "no"],
+                 "Starts": ["yes", 'no']}
+)
+
+car_model.add_cpds(cpd_starts, cpd_ignition, cpd_gas, cpd_radio, cpd_battery, cpd_moves, cpd_key_present)
+car_infer = VariableElimination(car_model)
+
+print("\n"
+      "-----------------------------------------\n"
+      "P(starts | gas, ignition, keyPresent)    \n"
+      "-----------------------------------------\n")
+print(car_infer.query(variables=["Starts"], evidence={"Gas": "Full", "Ignition": "Works", "KeyPresent": "yes"}))
+
+print("\n"
+      "-----------------------------------------\n"
+      "P(starts | gas, !ignition, keyPresent)    \n"
+      "-----------------------------------------\n")
+print(car_infer.query(variables=["Starts"], evidence={"Gas": "Full", "Ignition": "Doesn't work", "KeyPresent": "yes"}))
+
+print("\n"
+      "-----------------------------------------\n"
+      "P(starts | !gas, ignition, keyPresent)    \n"
+      "-----------------------------------------\n")
+print(car_infer.query(variables=["Starts"], evidence={"Gas": "Empty", "Ignition": "Works", "KeyPresent": "yes"}))
+
+print("\n"
+      "-----------------------------------------\n"
+      "P(starts | gas, ignition, !keyPresent)    \n"
+      "-----------------------------------------\n")
+print(car_infer.query(variables=["Starts"], evidence={"Gas": "Full", "Ignition": "Works", "KeyPresent": "no"}))
+
+print("\n"
+      "-----------------------------------------\n"
+      "P(starts | !gas, !ignition, keyPresent)    \n"
+      "-----------------------------------------\n")
+print(car_infer.query(variables=["Starts"], evidence={"Gas": "Empty", "Ignition": "Doesn't work", "KeyPresent": "yes"}))
+
+print("\n"
+      "-----------------------------------------\n"
+      "P(starts | !gas, ignition, !keyPresent)  \n"
+      "-----------------------------------------\n")
+print(car_infer.query(variables=["Starts"], evidence={"Gas": "Empty", "Ignition": "Works", "KeyPresent": "no"}))
+
+print("\n"
+      "-----------------------------------------\n"
+      "P(starts | gas, !ignition, !keyPresent)  \n"
+      "-----------------------------------------\n")
+print(car_infer.query(variables=["Starts"], evidence={"Gas": "Full", "Ignition": "Doesn't work", "KeyPresent": "no"}))
+
+print("\n"
+      "-----------------------------------------\n"
+      "P(starts | !gas, !ignition, !keyPresent)  \n"
+      "-----------------------------------------\n")
+print(car_infer.query(variables=["Starts"], evidence={"Gas": "Empty", "Ignition": "Doesn't work", "KeyPresent": "no"}))
 
